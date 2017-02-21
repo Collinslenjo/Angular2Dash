831fa77aed60c7193305d77def5665ea32359357
Collins Lenjo <collinslenjo.cl@gmail.com>
2017-02-21 19:14:31 +0300

    whatever that is to be styled overnight

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { ActivityComponent } from './activity/activity.component';
 import { SendMoneyComponent } from './send/send.component';
-import { HomeComponent } from './home/home.component';
 import { HelpComponent } from './help/help.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-import { AccountComponent } from './account/account.component';
 import { CustomerComponent } from './customer/customer.component';
 import { GuideComponent } from './guide/guide.component';
-import { GraphComponent } from './graph/graph.component';
 
 export const routes: Routes = [
   {path: 'login',component: LoginComponent},
