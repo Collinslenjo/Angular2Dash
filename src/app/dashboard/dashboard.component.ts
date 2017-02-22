424a73ffd732967253daf88f018dce7ee86f140d
collins lenjo <collinslenjo.cl@gmail.com>
2017-02-22 08:26:44 +0300

    copying

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,12 +3,15 @@ import { Http } from '@angular/http';
 import { Router } from '@angular/router';
 import { JwtHelper, AuthHttp } from 'angular2-jwt';
 
+import { ColorsService } from '../shared/colors/colors.service';
+//import map/rxjs;
+
 @Component({
   selector: 'dashboard',
   templateUrl: 'dashboard.component.html',
   styleUrls: ['dashboard.css']
 })
 
-export class DashboardComponent {
+export class DashboardComponent  {
 
 }
